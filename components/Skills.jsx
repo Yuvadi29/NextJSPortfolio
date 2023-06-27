7992e1f442b0c3de7115478baf4a0bfe85fe9f7b
Yuvadi29 <adityatrivedi612@gmail.com>
2023-06-27 17:57:24 +0530

    Removed Framer Motion for Mobile View for Skills

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -26,34 +26,59 @@ const Skills = () => {
     [200, 0] // Adjust the values based on the desired translation distance
   );
 
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
   return (
-    <div id='skills' className='w-full h-full lg:h-screen p-2 no-cursor'>
+    <div id='skills' className='w-full min-h-screen p-2 no-cursor'>
       <div className='max-w-[1240px] mx-auto flex flex-col justify-center h-full mt-56 md:mt-0'>
         {/* Skills */}
         <section className="w-full">
           <div className="font-bold text-white font-firaCode text-3xl lg:text-4xl text-center mb-4">
             ⚙ My Tech Stack
           </div>
-          <div className="m-auto grid lg:grid-cols-8 gap-2 md:grid-cols-5 sm:grid-cols-1">
+          <div className="m-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2">
             {skill.Skills.map((skills, index) => (
-              <motion.div
-                className="w-full md:w-[50%] flex"
-                initial="hidden"
-                animate={inViewImage ? "visible" : "hidden"}
-                variants={imageVariants}
-                ref={refImage}
+              <div
+                className={`w-full flex justify-center ${!isMobile && 'motion'}`}
                 key={index}
+                ref={refImage}
               >
-                <div className="flex justify-center">
-                  <Image src={skills.path} alt="myskills" width={800} height={800} className="w-full object-cover blur-sm hover:blur-none grayscale hover:grayscale-0 ml-[105px]" />
-                </div>
-              </motion.div>
+                {isMobile ? (
+                  <div className="w-full max-w-[200px]">
+                    <Image
+                      src={skills.path}
+                      alt="myskills"
+                      layout="responsive"
+                      width={200}
+                      height={200}
+                      className="blur-sm hover:blur-none grayscale hover:grayscale-0"
+                    />
+                  </div>
+                ) : (
+                  <motion.div
+                    initial="hidden"
+                    animate={inViewImage ? 'visible' : 'hidden'}
+                    variants={imageVariants}
+                  >
+                    <div className="w-full max-w-[200px]">
+                      <Image
+                        src={skills.path}
+                        alt="myskills"
+                        layout="responsive"
+                        width={200}
+                        height={200}
+                        className="blur-sm hover:blur-none grayscale hover:grayscale-0"
+                      />
+                    </div>
+                  </motion.div>
+                )}
+              </div>
             ))}
           </div>
         </section>
       </div>
     </div>
   );
-}
+};
 
 export default Skills;
